848d9d0da1cb302d5b8d5ae32a3e5d52dd44ec88
Mason duBoef <mduboef@gmail.com>
2020-12-28 12:32:43 -0700

    First commit
    Errors with datetime usage
    Autogui works

--- alarm.py
+++ alarm.py
@@ -1,35 +1,92 @@
-import os
-import sys
-import json
-import spotipy
-import webbrowser
-import spotipy.util as util
-from json.decoder import JSONDecodeError
+import pyautogui
+import time
+import datetime
+#from datetime import datetime
 
+alarmTime1 = datetime.time(9,29,00)
+debug = False
 
-# Get username
-# ID: mduboef?si=eBdxVVpoQzaCyIdJGIE00A
-username = sys.argv[1]
-#scope = 'playlist-read-private'
+alarmTime = alarmTime1.strftime("%H:%M")
+print(alarmTime)
 
-# Erase cashe and ask for user permission
-try:
-	token = util.prompt_for_user_token(username)
-except:
-	os.remove(f".cashe-{username}")
-	token = util.prompt_for_user_token(username)
+# infinite loop printing position for debug
+i = 1
+if debug == True:
+	while 1:
+		i += 1
+		print(pyautogui.position())
 
-app.get('/login', function(req, res) {
-var scopes = 'user-read-private user-read-email';
-res.redirect('https://accounts.spotify.com/authorize' +
-  '?response_type=code' +
-  '&client_id=' + my_client_id +
-  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-  '&redirect_uri=' + encodeURIComponent(redirect_uri));
-});
+# infinite loop for alarm running
+while 1:
 
+	# update current time
+	now = datetime.now()
+	timeNow = now.strftime("%H:%M")
+	
+	print(timeNow)
 
-spotifyObject = spotipy.Spotify.Spotify(auth=token)
+	# alarm time
+	if timeNow == alarmTime:
+		
+		#OPEN BROWSER
+		pyautogui.moveTo(20,20,1)
+		pyautogui.click()
+		pyautogui.moveTo(80,168,1)
+		pyautogui.moveTo(284,166,1)
+		pyautogui.click()
+		time.sleep(10)
+		
+		#OPEN DAILY DRIVE
+		pyautogui.moveTo(222,148,1)
+		pyautogui.click()
+		time.sleep(25)
+		
+		#PLAY SONG 1
+		pyautogui.moveTo(288,811,1)
+		pyautogui.click()
+		time.sleep(5)
+		
+		#QUEUE NPR NEWS
+		pyautogui.moveTo(1860,755,1)
+		pyautogui.click()
+		time.sleep(1)
+		pyautogui.moveTo(1730,780,1)
+		pyautogui.click()
+		time.sleep(5)
+		
+		#QUEUE SONG 2
+		pyautogui.moveTo(1860,866,1)
+		pyautogui.click()
+		time.sleep(1)
+		pyautogui.moveTo(1689,524,1)
+		pyautogui.click()
+		time.sleep(5)
 
-# spotify object
-spotifyObject = spotipy.Spotify(auth=token)
+		#OPEN NEW TAB
+		pyautogui.moveTo(269,80,1)
+		pyautogui.click()
+		time.sleep(10)
+		
+		#OPEN NYT DAILY
+		pyautogui.moveTo(155,148,1)
+		pyautogui.click()
+		time.sleep(25)
+		
+		#QUEUE NYT DAILY
+		pyautogui.moveTo(1214,759,1)
+		pyautogui.click()
+		time.sleep(1)
+		pyautogui.moveTo(1300,788,1)
+		pyautogui.click()
+		
+		pyautogui.moveTo(0,0,1)
+
+		#CLOSE BROWSER
+		time.sleep(5400)
+		pyautogui.moveTo(1905,49)
+		pyautogui.click()
+	
+	# if not alarm time wait 59 seconds
+	else:
+		time.sleep(59)
+	
